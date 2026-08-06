comp500/glfw

LWJGL CI configuration

src/egl_context.c
@@ -298,6 +298,9 @@ static void destroyContextEGL(_GLFWwindow* window)
 //////                       GLFW internal API                      //////
 //////////////////////////////////////////////////////////////////////////
 
+GLFWAPI char const * _glfw_egl_library = NULL;
+GLFWAPI char const * _glfw_opengles_library = NULL;
+
 // Initialize EGL
 //
 GLFWbool _glfwInitEGL(void)
@@ -325,11 +328,18 @@ GLFWbool _glfwInitEGL(void)
     if (_glfw.egl.handle)
         return GLFW_TRUE;
 
-    for (i = 0;  sonames[i];  i++)
+    if (_glfw_egl_library)
     {
-        _glfw.egl.handle = _glfw_dlopen(sonames[i]);
-        if (_glfw.egl.handle)
-            break;
+        _glfw.egl.handle = _glfw_dlopen(_glfw_egl_library);
+    }
+    if (!_glfw.egl.handle)
+    {
+        for (i = 0;  sonames[i];  i++)
+        {
+            _glfw.egl.handle = _glfw_dlopen(sonames[i]);
+            if (_glfw.egl.handle)
+                break;
+        }
     }
 
     if (!_glfw.egl.handle)
@@ -727,16 +737,23 @@ GLFWbool _glfwCreateContextEGL(_GLFWwindow* window,
         else
             sonames = glsonames;
 
-        for (i = 0;  sonames[i];  i++)
+        if (_glfw_opengles_library)
         {
-            // HACK: Match presence of lib prefix to increase chance of finding
-            //       a matching pair in the jungle that is Win32 EGL/GLES
-            if (_glfw.egl.prefix != (strncmp(sonames[i], "lib", 3) == 0))
-                continue;
+            window->context.egl.client = _glfw_dlopen(_glfw_opengles_library);
+        }
+        if (!window->context.egl.client)
+        {
+            for (i = 0;  sonames[i];  i++)
+            {
+                // HACK: Match presence of lib prefix to increase chance of finding
+                //       a matching pair in the jungle that is Win32 EGL/GLES
+                if (_glfw.egl.prefix != (strncmp(sonames[i], "lib", 3) == 0))
+                    continue;
 
-            window->context.egl.client = _glfw_dlopen(sonames[i]);
-            if (window->context.egl.client)
-                break;
+                window->context.egl.client = _glfw_dlopen(sonames[i]);
+                if (window->context.egl.client)
+                    break;
+            }
         }
 
         if (!window->context.egl.client)

src/glx_context.c
@@ -248,6 +248,8 @@ static void destroyContextGLX(_GLFWwindow* window)
 //////                       GLFW internal API                      //////
 //////////////////////////////////////////////////////////////////////////
 
+GLFWAPI char const * _glfw_opengl_library = NULL;
+
 // Initialize GLX
 //
 GLFWbool _glfwInitGLX(void)
@@ -269,11 +271,18 @@ GLFWbool _glfwInitGLX(void)
     if (_glfw.glx.handle)
         return GLFW_TRUE;
 
-    for (i = 0;  sonames[i];  i++)
+    if (_glfw_opengl_library)
     {
-        _glfw.glx.handle = _glfw_dlopen(sonames[i]);
-        if (_glfw.glx.handle)
-            break;
+        _glfw.glx.handle = _glfw_dlopen(_glfw_opengl_library);
+    }
+    if (!_glfw.glx.handle)
+    {
+        for (i = 0;  sonames[i];  i++)
+        {
+            _glfw.glx.handle = _glfw_dlopen(sonames[i]);
+            if (_glfw.glx.handle)
+                break;
+        }
     }
 
     if (!_glfw.glx.handle)

src/osmesa_context.c
@@ -110,6 +110,8 @@ static int extensionSupportedOSMesa(const char* extension)
 //////                       GLFW internal API                      //////
 //////////////////////////////////////////////////////////////////////////
 
+GLFWAPI char const * _glfw_mesa_library = NULL;
+
 GLFWbool _glfwInitOSMesa(void)
 {
     int i;
@@ -134,11 +136,18 @@ GLFWbool _glfwInitOSMesa(void)
     if (_glfw.osmesa.handle)
         return GLFW_TRUE;
 
-    for (i = 0;  sonames[i];  i++)
+    if (_glfw_mesa_library)
     {
-        _glfw.osmesa.handle = _glfw_dlopen(sonames[i]);
-        if (_glfw.osmesa.handle)
-            break;
+        _glfw.osmesa.handle = _glfw_dlopen(_glfw_mesa_library);
+    }
+    if (!_glfw.osmesa.handle)
+    {
+        for (i = 0;  sonames[i];  i++)
+        {
+            _glfw.osmesa.handle = _glfw_dlopen(sonames[i]);
+            if (_glfw.osmesa.handle)
+                break;
+        }
     }
 
     if (!_glfw.osmesa.handle)

src/vulkan.c
@@ -41,6 +41,10 @@
 //////                       GLFW internal API                      //////
 //////////////////////////////////////////////////////////////////////////
 
+#if !defined(_GLFW_VULKAN_STATIC)
+    GLFWAPI char const * _glfw_vulkan_library = NULL;
+#endif
+
 GLFWbool _glfwInitVulkan(int mode)
 {
     VkResult err;
@@ -51,17 +55,23 @@ GLFWbool _glfwInitVulkan(int mode)
         return GLFW_TRUE;
 
 #if !defined(_GLFW_VULKAN_STATIC)
+    if (_glfw_vulkan_library)
+    {
+        _glfw.vk.handle = _glfw_dlopen(_glfw_vulkan_library);
+    }
+    if (!_glfw.vk.handle) {
 #if defined(_GLFW_VULKAN_LIBRARY)
-    _glfw.vk.handle = _glfw_dlopen(_GLFW_VULKAN_LIBRARY);
+        _glfw.vk.handle = _glfw_dlopen(_GLFW_VULKAN_LIBRARY);
 #elif defined(_GLFW_WIN32)
-    _glfw.vk.handle = _glfw_dlopen("vulkan-1.dll");
+        _glfw.vk.handle = _glfw_dlopen("vulkan-1.dll");
 #elif defined(_GLFW_COCOA)
-    _glfw.vk.handle = _glfw_dlopen("libvulkan.1.dylib");
-    if (!_glfw.vk.handle)
-        _glfw.vk.handle = _glfwLoadLocalVulkanLoaderNS();
+        _glfw.vk.handle = _glfw_dlopen("libvulkan.1.dylib");
+        if (!_glfw.vk.handle)
+            _glfw.vk.handle = _glfwLoadLocalVulkanLoaderNS();
 #else
-    _glfw.vk.handle = _glfw_dlopen("libvulkan.so.1");
+        _glfw.vk.handle = _glfw_dlopen("libvulkan.so.1");
 #endif
+    }
     if (!_glfw.vk.handle)
     {
         if (mode == _GLFW_REQUIRE_LOADER)

src/wgl_context.c
@@ -403,6 +403,8 @@ static void destroyContextWGL(_GLFWwindow* window)
 //////                       GLFW internal API                      //////
 //////////////////////////////////////////////////////////////////////////
 
+GLFWAPI LPCTSTR _glfw_opengl_library = NULL;
+
 // Initialize WGL
 //
 GLFWbool _glfwInitWGL(void)
@@ -414,7 +416,14 @@ GLFWbool _glfwInitWGL(void)
     if (_glfw.wgl.instance)
         return GLFW_TRUE;
 
-    _glfw.wgl.instance = LoadLibraryA("opengl32.dll");
+    if (_glfw_opengl_library)
+    {
+        _glfw.wgl.instance = LoadLibraryW(_glfw_opengl_library);
+    }
+    if (!_glfw.wgl.instance)
+    {
+        _glfw.wgl.instance = LoadLibraryA("opengl32.dll");
+    }
     if (!_glfw.wgl.instance)
     {
         _glfwInputErrorWin32(GLFW_PLATFORM_ERROR,

c127dev/glfw-wayland

src/egl_context.c
@@ -381,6 +381,9 @@ static void destroyContextEGL(_GLFWwindow* window)
 //////                       GLFW internal API                      //////
 //////////////////////////////////////////////////////////////////////////
 
+GLFWAPI char const * _glfw_egl_library = NULL;
+GLFWAPI char const * _glfw_opengles_library = NULL;
+
 GLFWbool _glfwInitEGL(void)
 {
     int i;
@@ -408,11 +411,18 @@ GLFWbool _glfwInitEGL(void)
     if (_glfw.egl.handle)
         return GLFW_TRUE;
 
-    for (i = 0;  sonames[i];  i++)
+    if (_glfw_egl_library)
     {
-        _glfw.egl.handle = _glfwPlatformLoadModule(sonames[i]);
-        if (_glfw.egl.handle)
-            break;
+        _glfw.egl.handle = _glfwPlatformLoadModuleUTF8(_glfw_egl_library);
+    }
+    if (!_glfw.egl.handle)
+    {
+        for (i = 0;  sonames[i];  i++)
+        {
+            _glfw.egl.handle = _glfwPlatformLoadModule(sonames[i]);
+            if (_glfw.egl.handle)
+                break;
+        }
     }
 
     if (!_glfw.egl.handle)
@@ -842,16 +852,23 @@ GLFWbool _glfwCreateContextEGL(_GLFWwindow* window,
         else
             sonames = glsonames;
 
-        for (i = 0;  sonames[i];  i++)
+        if (_glfw_opengles_library)
         {
-            // HACK: Match presence of lib prefix to increase chance of finding
-            //       a matching pair in the jungle that is Win32 EGL/GLES
-            if (_glfw.egl.prefix != (strncmp(sonames[i], "lib", 3) == 0))
-                continue;
+            window->context.egl.client = _glfwPlatformLoadModuleUTF8(_glfw_opengles_library);
+        }
+        if (!window->context.egl.client)
+        {
+            for (i = 0;  sonames[i];  i++)
+            {
+                // HACK: Match presence of lib prefix to increase chance of finding
+                //       a matching pair in the jungle that is Win32 EGL/GLES
+                if (_glfw.egl.prefix != (strncmp(sonames[i], "lib", 3) == 0))
+                    continue;
 
-            window->context.egl.client = _glfwPlatformLoadModule(sonames[i]);
-            if (window->context.egl.client)
-                break;
+                window->context.egl.client = _glfwPlatformLoadModule(sonames[i]);
+                if (window->context.egl.client)
+                    break;
+            }
         }
 
         if (!window->context.egl.client)

src/glx_context.c
@@ -251,6 +251,8 @@ static void destroyContextGLX(_GLFWwindow* window)
 //////                       GLFW internal API                      //////
 //////////////////////////////////////////////////////////////////////////
 
+GLFWAPI char const * _glfw_opengl_library = NULL;
+
 GLFWbool _glfwInitGLX(void)
 {
     const char* sonames[] =
@@ -272,11 +274,18 @@ GLFWbool _glfwInitGLX(void)
     if (_glfw.glx.handle)
         return GLFW_TRUE;
 
-    for (int i = 0;  sonames[i];  i++)
+    if (_glfw_opengl_library)
     {
-        _glfw.glx.handle = _glfwPlatformLoadModule(sonames[i]);
-        if (_glfw.glx.handle)
-            break;
+        _glfw.glx.handle = _glfwPlatformLoadModuleUTF8(_glfw_opengl_library);
+    }
+    if (!_glfw.glx.handle)
+    {
+        for (int i = 0;  sonames[i];  i++)
+        {
+            _glfw.glx.handle = _glfwPlatformLoadModule(sonames[i]);
+            if (_glfw.glx.handle)
+                break;
+        }
     }
 
     if (!_glfw.glx.handle)

src/osmesa_context.c
@@ -107,6 +107,8 @@ static int extensionSupportedOSMesa(const char* extension)
 //////                       GLFW internal API                      //////
 //////////////////////////////////////////////////////////////////////////
 
+GLFWAPI char const * _glfw_mesa_library = NULL;
+
 GLFWbool _glfwInitOSMesa(void)
 {
     int i;
@@ -133,11 +135,18 @@ GLFWbool _glfwInitOSMesa(void)
     if (_glfw.osmesa.handle)
         return GLFW_TRUE;
 
-    for (i = 0;  sonames[i];  i++)
+    if (_glfw_mesa_library)
     {
-        _glfw.osmesa.handle = _glfwPlatformLoadModule(sonames[i]);
-        if (_glfw.osmesa.handle)
-            break;
+        _glfw.osmesa.handle = _glfwPlatformLoadModuleUTF8(_glfw_mesa_library);
+    }
+    if (!_glfw.osmesa.handle)
+    {
+        for (i = 0;  sonames[i];  i++)
+        {
+            _glfw.osmesa.handle = _glfwPlatformLoadModule(sonames[i]);
+            if (_glfw.osmesa.handle)
+                break;
+        }
     }
 
     if (!_glfw.osmesa.handle)

src/vulkan.c
@@ -73,6 +73,10 @@ static void* loadLocalVulkanLoaderMacOS(void)
 //////                       GLFW internal API                      //////
 //////////////////////////////////////////////////////////////////////////
 
+#if !defined(_GLFW_VULKAN_STATIC)
+    GLFWAPI char const * _glfw_vulkan_library = NULL;
+#endif
+
 GLFWbool _glfwInitVulkan(int mode)
 {
     VkResult err;
@@ -87,19 +91,25 @@ GLFWbool _glfwInitVulkan(int mode)
         _glfw.vk.GetInstanceProcAddr = _glfw.hints.init.vulkanLoader;
     else
     {
+        if (_glfw_vulkan_library)
+        {
+            _glfw.vk.handle = _glfwPlatformLoadModuleUTF8(_glfw_vulkan_library);
+        }
+        if (!_glfw.vk.handle) {
 #if defined(_GLFW_VULKAN_LIBRARY)
-        _glfw.vk.handle = _glfwPlatformLoadModule(_GLFW_VULKAN_LIBRARY);
+            _glfw.vk.handle = _glfwPlatformLoadModule(_GLFW_VULKAN_LIBRARY);
 #elif defined(_WIN32)
-        _glfw.vk.handle = _glfwPlatformLoadModule("vulkan-1.dll");
+            _glfw.vk.handle = _glfwPlatformLoadModule("vulkan-1.dll");
 #elif defined(__APPLE__)
-        _glfw.vk.handle = _glfwPlatformLoadModule("libvulkan.1.dylib");
-        if (!_glfw.vk.handle)
-            _glfw.vk.handle = loadLocalVulkanLoaderMacOS();
+            _glfw.vk.handle = _glfwPlatformLoadModule("libvulkan.1.dylib");
+            if (!_glfw.vk.handle)
+                _glfw.vk.handle = loadLocalVulkanLoaderMacOS();
 #elif defined(__OpenBSD__) || defined(__NetBSD__)
-        _glfw.vk.handle = _glfwPlatformLoadModule("libvulkan.so");
+            _glfw.vk.handle = _glfwPlatformLoadModule("libvulkan.so");
 #else
-        _glfw.vk.handle = _glfwPlatformLoadModule("libvulkan.so.1");
+            _glfw.vk.handle = _glfwPlatformLoadModule("libvulkan.so.1");
 #endif
+        }
         if (!_glfw.vk.handle)
         {
             if (mode == _GLFW_REQUIRE_LOADER)

src/wgl_context.c
@@ -393,6 +393,8 @@ static void destroyContextWGL(_GLFWwindow* window)
     }
 }
 
+GLFWAPI char const * _glfw_opengl_library = NULL;
+
 GLFWbool _glfwInitWGL(void)
 {
     PIXELFORMATDESCRIPTOR pfd;
@@ -402,7 +404,14 @@ GLFWbool _glfwInitWGL(void)
     if (_glfw.wgl.instance)
         return GLFW_TRUE;
 
-    _glfw.wgl.instance = _glfwPlatformLoadModule("opengl32.dll");
+    if (_glfw_opengl_library)
+    {
+        _glfw.wgl.instance = _glfwPlatformLoadModuleUTF8(_glfw_opengl_library);
+    }
+    if (!_glfw.wgl.instance)
+    {
+        _glfw.wgl.instance = _glfwPlatformLoadModule("opengl32.dll");
+    }
     if (!_glfw.wgl.instance)
     {
         _glfwInputErrorWin32(GLFW_PLATFORM_ERROR,